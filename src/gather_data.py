c8139ec36269777bbc81a7e6797f09123d40bb23
Spandan Garg <spandan.garg1706@gmail.com>
2019-06-05 10:45:01 -0400

    add required and optional tag functionality to data collection

--- src/gather_data.py
+++ src/gather_data.py
@@ -5,8 +5,6 @@ import util
 
 from time import sleep
 
-#TODO: Find a way to create query with optional and required topics, e.g. (Machine Learning AND Computer Science) OR Natural Language Processing
-
 class CoreAPIDataCollector():
 	def __init__(self, core_api_access_token=None):
 		import requests
@@ -16,22 +14,28 @@ class CoreAPIDataCollector():
 
 		self.core_api_access_token = core_api_access_token
 
-		request_retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
+		request_retries = Retry(total=20, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
 		self.request_session = requests.Session()
 		self.request_session.mount('http://', HTTPAdapter(max_retries=request_retries))
 
-	def _create_query_for_topics(self, topics):
-		topic_queries = []
-		for topic in topics:
+	def _create_query_for_topics(self, required_topics, optional_topics=None):
+		def create_query_for_topic(topic):
 			keywords = ' AND '.join(topic.split())
-			topic_queries += ['(title:({0}) OR description:({0}) OR fullText:({0}))'.format(keywords)]
+			return '(title:({0}) OR description:({0}) OR fullText:({0}))'.format(keywords)
 
-		return '({})'.format(' OR '.join(topic_queries))
+		required_topic_queries = [create_query_for_topic(topic) for topic in required_topics]
+		if optional_topics is not None:
+			optional_topic_queries = [create_query_for_topic(topic) for topic in optional_topics]
+			optional_query = '({})'.format(' OR '.join(optional_topic_queries))
+			required_topic_queries += [optional_query]
 
-	def get_articles_for_topic(self, topic):
+		query = '({})'.format(' AND '.join(required_topic_queries))
+		return query
+
+	def get_articles_for_topics(self, required_topics, optional_topics=None):
 		import urllib.parse
 
-		query = self._create_query_for_topics([topic])
+		query = self._create_query_for_topics(required_topics, optional_topics)
 		request_url = 'https://core.ac.uk:443/api-v2/articles/search/{}'.format(urllib.parse.quote(query))
 		params = {
 			'pageSize': 100,
@@ -72,7 +76,8 @@ class CoreAPIDataCollector():
 
 def main(args):
 	data_collector = CoreAPIDataCollector(args.core_api_access_token)
-	articles = data_collector.get_articles_for_topic('machine learning')
+	# assuming core api is NOT case sensitive
+	articles = data_collector.get_articles_for_topics(['machine learning'], ['natural language processing', 'nlp', 'language model'])
 	util.dump_json(articles, args.output_articles_json_path)
 
 if __name__ == '__main__':
