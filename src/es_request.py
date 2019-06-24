d5a5f1c1312e748c32f45c477b1bafc981235266
Andy Lee <leechangho@hotmail.com>
2019-06-24 09:27:26 -0700

    fix port number:

--- src/es_request.py
+++ src/es_request.py
@@ -6,7 +6,7 @@ es = Elasticsearch(
     ['https://b5d9b60753c1475b813fa4416dec2cbe.us-east-1.aws.found.io'],
     http_auth=('elastic', 'YfCzcnEoTbbkbXV9zM5kH8MN'),
     scheme="https",
-    port=443
+    port=9243
 )
 
 print(es.ping())
