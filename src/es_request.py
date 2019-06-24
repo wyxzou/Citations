32c55b91c0b271819a3d2ad2cd405fc2b6af0a0d
Andy Lee <leechangho@hotmail.com>
2019-06-24 09:58:46 -0700

    Update using local es

--- src/es_request.py
+++ src/es_request.py
@@ -1,12 +1,30 @@
-from elasticsearch import Elasticsearch
-import requests
+from elasticsearch import Elasticsearch, helpers
+import logging
 import json
 
-es = Elasticsearch(
-    ['https://b5d9b60753c1475b813fa4416dec2cbe.us-east-1.aws.found.io'],
-    http_auth=('elastic', 'YfCzcnEoTbbkbXV9zM5kH8MN'),
-    scheme="https",
-    port=9243
-)
+def connect_elasticsearch():
+    _es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    if _es.ping():
+        print('Yay Connect')
+    else:
+        print('Awww it could not connect!')
+    return _es
 
-print(es.ping())
+def gendata():
+    json_items = []
+    with open('../repository_metadata_2013-12-15/repository_metadata_2013-12-09_2.json') as json_file:  
+        for line in json_file:
+            j = json.loads(line)
+            json_items.append(j)
+    for json_item in json_items:
+        print(json_item["identifier"])
+        yield {
+            "_index": "id",
+            "_type": "document",
+            "doc": {"word": json_item["identifier"]},
+        }
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.ERROR)
+  es = connect_elasticsearch()
+  helpers.bulk(es, gendata())
