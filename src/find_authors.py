da1791aa7f1009fb3a2dfaa4f95303ddb8ccbd46
William Zou <wyxzou@gmail.com>
2019-07-04 02:11:35 -0400

    check if all references exists

--- src/find_authors.py
+++ src/find_authors.py
@@ -2,6 +2,17 @@ import es_request
 import logging
 
 
+# ground truth and pool are a list of ids
+def num_included(ground_truth, pool):
+    count = 0
+    for i in ground_truth:
+        if i in pool:
+            count += 1
+
+    return count / len(ground_truth)
+
+
+# matching authors.name is not necessary, feel free to remove it
 def find_author(author_id, author_name):
     logging.basicConfig(level=logging.ERROR)
     es = es_request.connect_elasticsearch()    
@@ -20,5 +31,28 @@ def find_author(author_id, author_name):
     return res
 
 
+def if_references_exist(paper_id):
+    logging.basicConfig(level=logging.ERROR)
+    es = es_request.connect_elasticsearch()   
+    res = es.search(index="aminer", body={"query": {"match" : {"id": paper_id}}})
+    
+    references = res['hits']['hits'][0]['_source']['references']
+
+    if not references:
+        return 1
+
+    count = 0
+    for r in references:
+        res = es.search(index="aminer", body={"query": {"match" : {"id": r}}})
+        if not res['hits']['hits']:
+            count += 1
+
+    return (len(references) - count) / len(references)
+
+
+
 if __name__ == '__main__':
     print(find_author('2702511795', 'Peter Kostelnik'))
+    print(if_references_exist('100008749'))
+
+    # print(es.search(index="aminer", body={"query": {"match" : {"id": "ab"}}})['hits']['hits'])
