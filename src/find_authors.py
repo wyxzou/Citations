dc0b55d3b263fdfc95248d596a359deea84c83f6
William Zou <wyxzou@gmail.com>
2019-07-28 13:17:50 -0400

    Update find_authors to grab articles that share the same author

--- src/find_authors.py
+++ src/find_authors.py
@@ -1,5 +1,6 @@
 import es_request
 import logging
+import json
 
 
 # ground truth and pool are a list of ids
@@ -12,8 +13,25 @@ def num_included(ground_truth, pool):
     return count / len(ground_truth)
 
 
+# given a paper, return a list of author ids associated with that paper
+def find_authors(pid):
+    logging.basicConfig(level=logging.ERROR)
+    es = es_request.connect_elasticsearch()  
+
+    res = es.search(index="aminer", body={"query": {"match" : {"id": pid}}})
+    
+    author_list = res['hits']['hits'][0]['_source']['authors']
+
+    author_ids = []
+    for author in author_list:
+
+        author_ids.append(author['id'])
+
+    return author_ids
+
+
 # matching authors.name is not necessary, feel free to remove it
-def find_author(author_id):
+def find_papers_with_author_id(author_id):
     logging.basicConfig(level=logging.ERROR)
     es = es_request.connect_elasticsearch()    
 
@@ -61,11 +79,31 @@ def if_references_exist(paper_id):
 
 
 if __name__ == '__main__':
-    res = find_author('2702511795') # 'Peter Kostelnik')
+    with open('../ids.txt') as f:
+        paper_ids = f.read().splitlines()
 
-    print(get_ids(res))
-    print(if_references_exist('100008749'))
+    # paper_ids = ['100008749']
 
+    # r = find_papers_with_author_id('2702511795')
+    # print(r['hits']['hits'][0])
+    # print(r['hits']['hits'][1])
+    # print(get_ids(r))
+    
+    filename = "../shared_authors.json"
+    dic = {}
+    for pid in paper_ids:
+        author_ids = find_authors(pid)
+        recommended = []
+        for a_id in author_ids:
+            res = find_papers_with_author_id(a_id) # 'Peter Kostelnik')
+            papers_with_shared_author = get_ids(res)
+            recommended.extend(list(set(papers_with_shared_author)))
 
+        recommended = list(set(recommended))
+        recommended.remove(pid)
+        dic[pid] = recommended
 
-    # print(es.search(index="aminer", body={"query": {"match" : {"id": "ab"}}})['hits']['hits'])
+    with open(filename, mode='w', encoding='utf-8') as feedsjson:        
+        json.dump(dic, feedsjson)
+
+    # print(if_references_exist('100008749'))
