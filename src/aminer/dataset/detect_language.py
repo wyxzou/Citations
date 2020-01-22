5c13c62c544b6a0e7a207c38209197d0ad3b637d
William Zou <wyxzou@gmail.com>
2020-01-22 01:03:18 -0500

    Add counter to keep track of language detection progress.

--- src/aminer/dataset/detect_language.py
+++ src/aminer/dataset/detect_language.py
@@ -39,16 +39,18 @@ if __name__ == '__main__':
     es = es_request.connect_elasticsearch()
     res = es.search(index="aminer", body={
         "_source": ["id", "abstract", "title"],
-        "size": 10000,
+        "size": 5000,
         "query": {
             "match_all": {}
         }
     }, scroll='2m')
 
     # get es scroll id
+    count = 0
     scroll_id = res['_scroll_id']
     while res['hits']['hits'] and len(res['hits']['hits']) > 0:
         for hit in res['hits']['hits']:
+            print('Update pid: ', pid)
             pid = hit['_source']['id']
             abstract = hit['_source']['abstract']
             title = hit['_source']['title']
@@ -72,7 +74,9 @@ if __name__ == '__main__':
             es = es_request.connect_elasticsearch()
             response = es.update(index="aminer", id=pid, body=dic)
 
-        # get es scroll id
+        count += 1
+        print('Completed ', count, ' batches')
+        # get es scroll idsss
         scroll_id = res['_scroll_id']
         # use es scroll api
         res = es.scroll(scroll_id=scroll_id, scroll='2m', request_timeout=10)
