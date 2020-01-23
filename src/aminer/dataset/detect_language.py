626caef929870077199315a1f3a2c1f92cfa0294
William Zou <wyxzou@gmail.com>
2020-01-23 17:49:05 -0500

    Remove duplicated script from detect_language.py.

--- src/aminer/dataset/detect_language.py
+++ src/aminer/dataset/detect_language.py
@@ -98,6 +98,7 @@ def update_language_all_from_directory(start=0):
     mypath = join(support_directory, 'abstract_dictionaries')
     files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     num_files = len(files)
+    es = es_request.connect_elasticsearch()
 
     for i in range(start, num_files):
         filename = 'abstract_dict_' + str(i) + '.json'
@@ -138,66 +139,10 @@ def update_language_all_from_directory(start=0):
                         "language": lang,
                     }
                 }
-
                 response = es.update(index="aminer", id=pid, body=dic)
 
         print('Completed: ', filename)
 
 
 if __name__ == '__main__':
-    es = es_request.connect_elasticsearch()
-    res = es.search(index="aminer", body={
-        "_source": ["id", "abstract", "title"],
-        "size": 5000,
-        "query": {
-            "match_all": {}
-        }
-    }, scroll='20m')
-
-    # get es scroll id
-    count = 0
-    scroll_id = res['_scroll_id']
-    while res['hits']['hits'] and len(res['hits']['hits']) > 0:
-        for hit in res['hits']['hits']:
-            pid = hit['_source']['id']
-            abstract = hit['_source']['abstract']
-            title = hit['_source']['title']
-
-            etp = EnglishTextProcessor()
-            processed_abstract = etp(abstract)
-
-            if len(processed_abstract) > 0:
-                try:
-                    lang = detect(processed_abstract)
-                except:
-                    lang = 'Language cannot be detected'
-                    print('The language of this abstract cannot be detected: ', processed_abstract)
-                    print('The paper id is: ', pid)
-            elif len(title) > 0:
-                try:
-                    lang = detect(title)
-                except:
-                    lang = 'Language cannot be detected'
-                    print('The language of this title cannot be detected: ', title)
-                    print('The paper id is: ', pid)
-            else:
-                lang = 'Empty abstract and title'
-
-            dic = {
-                'doc': {
-                    "language": lang,
-                }
-            }
-
-            # es2 = es_request.connect_elasticsearch()
-            response = es.update(index="aminer", id=pid, body=dic)
-
-    count += 1
-    print('Completed ', count, ' batches')
-    # get es scroll ids
-    scroll_id = res['_scroll_id']
-    # use es scroll api
-    res = es.scroll(scroll_id=scroll_id, scroll='20m', request_timeout=10)
-
-
-    
+    update_language_all_from_directory(0)
