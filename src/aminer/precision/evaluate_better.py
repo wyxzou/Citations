ee8f6f0cf4db1dcb445f702433037683ea68f010
EC2 Default User <ec2-user@ip-172-31-5-69.us-east-2.compute.internal>
2020-01-26 07:20:20 +0000

    Add faster script

---
+++ src/aminer/precision/evaluate_better.py
@@ -0,0 +1,108 @@
+import heapq
+import json
+import math
+import os
+import pickle
+from os import listdir
+from os.path import isfile
+
+import numpy as np
+import pkg_resources
+import scipy
+from gensim.models import FastText
+from scipy import spatial
+from tqdm import tqdm
+
+from aminer.recall.query_es import get_abstract_by_pids, get_references_by_pid
+from aminer.precision.metrics import recall, precision
+
+root_directory = pkg_resources.resource_filename("aminer", "support")
+model_directory = os.path.join(root_directory, "models")
+
+vec = pickle.load(open(os.path.join(model_directory, 'vec.model'), 'rb'))
+fasttext = FastText.load(os.path.join(model_directory, 'fasttext', 'fasttext.model'))
+word_to_idx = list(vec.get_feature_names())
+
+
+def compute_abstract_embedding(abstract, is_query=False):
+    tfidf_vec = vec.transform([abstract])
+    tfidf_vec = scipy.sparse.coo_matrix(tfidf_vec)
+    word_count = 0
+    sum_embedding = np.zeros(50)
+    for _, word_index, word_tfidf in zip(tfidf_vec.row, tfidf_vec.col, tfidf_vec.data):
+        word = word_to_idx[word_index]
+        if word in fasttext.wv.vocab:
+            word_count += 1
+            if not is_query:
+                sum_embedding += word_tfidf * fasttext.wv[word]
+            else:
+                sum_embedding += fasttext.wv[word]
+
+    if word_count == 0:
+        return [0] * 50
+
+    return (sum_embedding / word_count).tolist()
+
+
+def get_similarities(dataset_embedding, test_embeddings):
+    # TODO: find a way to do this in one line
+    similarities = []
+    for embedding in test_embeddings:
+        similarities += [1 - spatial.distance.cosine(embedding, dataset_embedding)]
+
+    return similarities
+
+
+def recommend(ids, k=100):
+    ids_to_abstract = get_abstract_by_pids(ids)
+    ids_to_embeddings = {id:compute_abstract_embedding(ids_to_abstract[id]) for id in ids_to_abstract}
+    test_embeddings = [ids_to_embeddings[id] for id in ids_to_embeddings]
+    recommendations = {id: [] for id in ids_to_embeddings}
+
+    embeddings_directory = os.path.join(root_directory, "output_embeddings2")
+    files = [os.path.join(embeddings_directory, f) for f in listdir(embeddings_directory) if isfile(os.path.join(embeddings_directory, f))]
+    for file in tqdm(files):
+        with open(file, 'r') as f:
+            embeddings = json.load(f)
+            f.close()
+
+        for id, emb in embeddings.items():
+            similarities = get_similarities(emb, test_embeddings)
+            for example_id, similarity in zip(list(ids_to_embeddings.keys()), similarities):
+                if len(recommendations[example_id]) < k:
+                    # grow the heap
+                    heapq.heappush(recommendations[example_id], tuple([similarity, id]))
+                else:
+                    # maintain the size
+                    heapq.heappushpop(recommendations[example_id], tuple([similarity, id]))
+
+    # discard similarities and only keep id from the tuples
+    recommendations = {id: [a[1] for a in recommendations[id]] for id in recommendations}
+    return recommendations
+
+
+if __name__ == '__main__':
+    file = os.path.join(root_directory, 'ids.txt')
+    ids = [line.rstrip('\n') for line in open(file)]
+
+    recommendations = recommend(ids, 100000)
+
+    outfile = 'test_recommendations.json'
+
+    with open(outfile, 'w') as f:
+        json.dump(recommendations, f)
+
+    for pid, recs in recommendations.items():
+        pred_reference_list = [e[0] for e in recs]
+        true_reference_list = get_references_by_pid(pid)
+
+        print(precision(pred_reference_list, true_reference_list))
+        print(recall(pred_reference_list, true_reference_list))
+
+
+
+
+
+
+
+
