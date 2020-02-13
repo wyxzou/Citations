7f679b40e69fca95bd188b79942f0fdc3f3200f0
EC2 Default User <ec2-user@ip-172-31-5-69.us-east-2.compute.internal>
2020-02-13 01:55:24 +0000

    update evaluate_fos

--- src/aminer/precision/evaluate_fos.py
+++ src/aminer/precision/evaluate_fos.py
@@ -9,7 +9,7 @@ from os.path import isfile
 import numpy as np
 import pkg_resources
 import scipy
-from gensim.models import FastText
+from gensim.models import FastText, Word2Vec
 from scipy import spatial
 from tqdm import tqdm
 
@@ -20,10 +20,12 @@ root_directory = pkg_resources.resource_filename("aminer", "support")
 model_directory = os.path.join(root_directory, "models")
 
 vec = pickle.load(open(os.path.join(model_directory, 'vec.model'), 'rb'))
-fasttext = FastText.load(os.path.join(model_directory, 'fasttext', 'fasttext.model'))
+fasttext = Word2Vec.load('word2vec_100/word2vec_100.model')
 word_to_idx = list(vec.get_feature_names())
 relevant_ids = None
-with open('fos_filtered_recommendations.json') as relevant_ids_file:
+dim = 100
+filter_method = 'fos_author_filtered.json'
+with open(filter_method) as relevant_ids_file:
     relevant_ids = json.load(relevant_ids_file)
 
 for id in relevant_ids:
@@ -33,7 +35,7 @@ def compute_abstract_embedding(abstract, is_query=False):
     tfidf_vec = vec.transform([abstract])
     tfidf_vec = scipy.sparse.coo_matrix(tfidf_vec)
     word_count = 0
-    sum_embedding = np.zeros(50)
+    sum_embedding = np.zeros(dim)
     for _, word_index, word_tfidf in zip(tfidf_vec.row, tfidf_vec.col, tfidf_vec.data):
         word = word_to_idx[word_index]
         if word in fasttext.wv.vocab:
@@ -44,7 +46,7 @@ def compute_abstract_embedding(abstract, is_query=False):
                 sum_embedding += fasttext.wv[word]
 
     if word_count == 0:
-        return [0] * 50
+        return [0] * dim
 
     return (sum_embedding / word_count).tolist()
 
@@ -56,7 +58,7 @@ def get_all_embeddings(test_id, current_embedding, files):
             f.close()
 
         for id, emb in embeddings.items():
-            if id not in relevant_ids[test_id]:
+            if str(id) not in relevant_ids[test_id]:
                 yield (id, -1)
             else:
                 cosine_similarity = 1 - spatial.distance.cosine(current_embedding, emb)
@@ -71,7 +73,7 @@ def recommend(ids, k=100):
 
     for i, (target_id, abstract) in enumerate(ids_to_abstract.items()):
         print(i, "Finding recommendations for: ", target_id)
-        embeddings_directory = os.path.join(root_directory, "output_embeddings2")
+        embeddings_directory = 'word2vec_100_abstract_dictionaries'
 
         current_embedding = compute_abstract_embedding(abstract, is_query=True)
 
@@ -87,20 +89,25 @@ if __name__ == '__main__':
     file = os.path.join(root_directory, 'ids.txt')
     ids = [line.rstrip('\n') for line in open(file)]
 
-    recommendations = recommend(ids, 100000)
+    #recommendations = recommend(ids, 100000)
 
-    outfile = os.path.join(root_directory, 'recommendations.json')
+    outfile = os.path.join('r_word2vec_100_fos_author.json')
+    recommendations = {}
+    with open(outfile, 'r') as f:
+        #json.dump(recommendations, f)
+        recommendations = json.load(f)
 
-    with open(outfile, 'w') as f:
-        json.dump(recommendations, f)
-
+    rec = 0
     for pid, recs in recommendations.items():
-        pred_reference_list = [e[0] for e in recs]
+        pred_reference_list = [e[0] for e in recs[:50]]
         true_reference_list = get_references_by_pid(pid)
 
         print(precision(pred_reference_list, true_reference_list))
-        print(recall(pred_reference_list, true_reference_list))
+        example_rec = recall(pred_reference_list, true_reference_list)
+        rec += example_rec
+        print(example_rec)
 
+    print('Overall Recall: ', rec / len(recommendations))
 
 
 
