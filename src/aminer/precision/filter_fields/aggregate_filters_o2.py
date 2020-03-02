9e530cc6347e5392ade70b6e3ad431cadf2d5ad3
William Zou <wyxzou@gmail.com>
2020-03-02 01:51:01 -0500

    Add filter fields for o2.

--- src/aminer/precision/filter_fields/aggregate_filters_o2.py
+++ src/aminer/precision/filter_fields/aggregate_filters_o2.py
@@ -9,11 +9,12 @@ directory_2019 = os.path.join(support_directory, "2019_filters")
 fos = load_json(os.path.join(directory_2019, 'fos_filtered_recommendations.json'))
 author = load_json(os.path.join(support_directory, 'author_filtered_citations_o2.json'))
 years = load_json(os.path.join(support_directory, 'invalid_citations_based_on_year.json'))
+language = load_json(os.path.join(directory_2019, 'invalid_citations_based_on_language.json'))
 
 both = {}
 for id in fos:
     if id in author:
-        both[id] = list(set(author[id]).intersection(set(fos[id])) - set(years))
+        both[id] = list(set(author[id]).intersection(set(fos[id])) - set(years) - set(language))
     else:
         both[id] = list(set(fos[id]) - set(years))
 
